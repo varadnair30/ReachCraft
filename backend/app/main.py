dc0d872bce787b4f0732b4c1104183173b574c3c
Varad <vnvarad.30@gmail.com>
2025-12-26 17:52:31 -0600

    fix: update CORS to allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000', 'http://localhost:5173', 'chrome-extension://*', "https://*.onrender.com", 'null'],
+    allow_origins=['http://localhost:3000', 'http://localhost:5173',  '*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
